2f298b880e0f6d670a6765c6fbf87a61507afc4d
kongyi123 <kongyi123@nate.com>
2021-10-26 03:02:31 +0900

    fix some file names properly

---
+++ sharecalendar/src/main/java/com/example/sharecalendar/DayActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sharecalendar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_day)
+    }
+}
