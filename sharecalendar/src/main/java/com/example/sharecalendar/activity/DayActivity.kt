581943309bf921a1187cc9363c4228160dc184e6
kongyi123 <kongyi123@nate.com>
2021-11-11 01:05:34 +0900

    changed button position of DayActivity

--- sharecalendar/src/main/java/com/example/sharecalendar/activity/DayActivity.kt
+++ sharecalendar/src/main/java/com/example/sharecalendar/activity/DayActivity.kt
@@ -1,12 +1,10 @@
 package com.example.sharecalendar.activity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.CalendarView
 import android.widget.DatePicker
 import android.widget.EditText
 import com.example.sharecalendar.DataManager
